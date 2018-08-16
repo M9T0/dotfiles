d94fc681ab56309e9afed7aa5a34140e2e970177
Makoto Ito <ito.makoto@interlink-j.com>
2018-08-16 17:34:34 +0900

    * config.fish: 追加

---
+++ .config/fish/config.fish
@@ -0,0 +1,5 @@
+set -x PATH /home/linuxbrew/.linuxbrew/bin $PATH
+set -x PATH $HOME/go/bin $PATH
+set -x PATH $HOME/.roswell/bin $PATH
+set -x LD_LIBRARY_PATH /home/linuxbrew/.linuxbrew/lib $LD_LIBRARY_PATH
+set -x DISPLAY localhost:0.0

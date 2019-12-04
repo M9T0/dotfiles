d2f3adb9afe4fb87e2acc2b5ca7a08c969605c8d
Makoto Ito <m4k070@outlook.jp>
2019-12-04 14:30:20 +0900

    エディタ環境変数追加

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,7 +4,7 @@ set -x PATH $HOME/.cargo/bin $PATH
 set -x PATH $HOME/.cargo/bin $PATH
 set -x XMODIFIERS "@im=uim"
 set -x LD_LIBRARY_PATH /home/linuxbrew/.linuxbrew/lib $LD_LIBRARY_PATH
-set -x DISPLAY localhost:0.0
+set -x EDITOR vim
 
 alias ls exa
 alias cat bat
